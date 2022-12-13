0294bd237919cef629c3f0b9e20217fa7d890397
Jéssica Xavier Gravina <116176017+JessicaXavierGravina@users.noreply.github.com>
2022-12-12 23:18:44 -0300

    Add files via upload
    etapas do CRUD,  com python, interagindo  com banco de dados MySQL usando python.

---
+++ mysql_python_.py
@@ -0,0 +1,45 @@
+#  pip install mysql-connector-python
+
+import mysql.connector
+
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='123456',
+    database='bdempresa',
+)
+cursor = conexao.cursor()
+
+# CRUD
+#CREATE
+nome_produto = "pao de queijo"
+valor = 15
+comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+cursor.execute(comando)
+conexao.commit() # edita o banco de dados
+
+
+# READ
+# comando = f'SELECT * FROM vendas'
+# cursor.execute(comando)
+# resultado = cursor.fetchall() # le o banco de dados
+# print(resultado)
+
+
+# UPDATE
+# nome_produto = "geleia"
+# valor = 6
+# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit() # edita o banco de dados
+
+# DELETE
+# nome_produto = "geleia"
+# comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit() # edita o banco de dados
+
+cursor.close()
+conexao.close()
